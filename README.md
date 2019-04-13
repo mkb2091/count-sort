8e5a7a9d4c7e80548ea18973650555a2d00d70ff
Alex Williams <hughaw64@gmail.com>
2019-04-13 17:13:37 +0100

    Optimised
    Slight loop unrolling

--- README.md
+++ README.md
@@ -14,17 +14,17 @@ Time to clone and sort 10 randomly generated Vec<u8> of length specified below
 
 | length | .sort()   | sort_u8   |
 |--------|-----------|-----------|
-| 0      | 77.827 ns | 2.2589 us |
-| 1      | 254.34 ns | 2.4830 us |
-| 4      | 311.05 ns | 2.8181 us |
-| 16     | 1.0484 us | 3.4905 us |
-| 64     | 14.810 us | 5.8713 us |
-| 256    | 111.79 us | 11.704 us |
-| 1024   | 632.43 us | 53.338 us |
-| 4096   | 3.1092 ms | 86.189 us |
-| 16384  | 11.990 ms | 244.76 us |
-| 65536  | 52.786 ms | 968.00 us |
-| 262144 | 219.08 ms | 3.8404 ms |
+| 0      | 77.827 ns | 2.2363 us |
+| 1      | 254.34 ns | 2.4040 us |
+| 4      | 311.05 ns | 2.6972 us |
+| 16     | 1.0484 us | 3.2623 us |
+| 64     | 14.810 us | 5.2870 us |
+| 256    | 111.79 us | 11.182 us |
+| 1024   | 632.43 us | 51.417 us |
+| 4096   | 3.1092 ms | 84.417 us |
+| 16384  | 11.990 ms | 251.44 us |
+| 65536  | 52.786 ms | 906.54 us |
+| 262144 | 219.08 ms | 3.6449 ms |
 
 ![Default Sort](benches/default_sort_report.svg)
 ![count_sort::sort_u8](benches/sort_u8_report.svg)
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 pub fn sort_u8(array: &mut Vec<u8>) {
     if array.len() < 256 {
         let mut table: [u8; 256] = [0; 256];
-        for i in array.iter() {
-            table[*i as usize] += 1;
+        let mut x = 0;
+        if array.len() % 2 == 1 {
+            table[array[0] as usize] += 1;
+            x += 1;
+        }
+        while x < array.len() {
+            table[array[x] as usize] += 1;
+            table[array[x + 1] as usize] += 1;
+            x += 2;
         }
         let mut pos = 0;
         for (i, amount) in table.iter().enumerate().filter(|(_, &amount)| amount > 0) {
@@ -13,8 +20,15 @@ pub fn sort_u8(array: &mut Vec<u8>) {
         }
     } else if array.len() < 65536 {
         let mut table: [u16; 256] = [0; 256];
-        for i in array.iter() {
-            table[*i as usize] += 1;
+        let mut x = 0;
+        if array.len() % 2 == 1 {
+            table[array[0] as usize] += 1;
+            x += 1;
+        }
+        while x < array.len() {
+            table[array[x] as usize] += 1;
+            table[array[x + 1] as usize] += 1;
+            x += 2;
         }
         let mut pos = 0;
         for (i, amount) in table.iter().enumerate().filter(|(_, &amount)| amount > 0) {
@@ -25,8 +39,15 @@ pub fn sort_u8(array: &mut Vec<u8>) {
         }
     } else {
         let mut table: [usize; 256] = [0; 256];
-        for i in array.iter() {
-            table[*i as usize] += 1;
+        let mut x = 0;
+        if array.len() % 2 == 1 {
+            table[array[0] as usize] += 1;
+            x += 1;
+        }
+        while x < array.len() {
+            table[array[x] as usize] += 1;
+            table[array[x + 1] as usize] += 1;
+            x += 2;
         }
         let mut pos = 0;
         for (i, amount) in table.iter().enumerate().filter(|(_, &amount)| amount > 0) {
